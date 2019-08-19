23fe387d89a855291f14accfd441102323f22197
Tam To Tran <tamtotran5667@gmail.com>
2019-08-20 00:37:41 +0700

    add line searchText

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -14,6 +14,7 @@ export class ProductsComponent implements OnInit {
   changeStatus() {
     this.visibleImage = !this.visibleImage;
   }
+  searchText;
   visibleImage = true;
   listProducts = [
     {
