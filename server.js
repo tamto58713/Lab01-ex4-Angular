c91f84afeb51a1b794a13a121b5878a5f14b5d5a
Tam To Tran <tamtotran5667@gmail.com>
2019-08-20 03:36:45 +0700

    fix lap01 to Lap01

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/lap01'));
+app.use(express.static(__dirname + '/dist/Lap01'));
+
 
 app.get('/*', function(req,res) {
     
