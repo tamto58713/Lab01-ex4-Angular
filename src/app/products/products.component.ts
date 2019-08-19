5ffece51fea84cfcf8b0ea7503c208646e428833
Tam To Tran <tamtotran5667@gmail.com>
2019-08-19 22:55:00 +0700

    add data of products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,48 +11,53 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  test="Testing"
   listProducts = [
     {
+      id: 1,
       productName: "Leaf Rake",
       code: "GDN-0011",
       available: "Mar 19, 2016",
       price: "19.95",
       starRating: "0.62",
-      ImageURL: "../assets/images/leaf-rake.jpg"
+      imageURL: "../assets/images/leaf-rake.jpg"
     },
     {
+      id: 2,
       productName: "Garden Cart",
       code: "GDN-0023",
       available: "Mar 18, 2016",
       price: "32.99",
       starRating: "0.81",
-      ImageURL: "../assets/images/garden-cart.jpg"
+      imageURL: "../assets/images/garden-cart.jpg"
     },
     {
+      id: 3,
       productName: "Hammer",
       code: "TBX-0048",
       available: "Mar 21, 2016",
       price: "8.99",
       starRating: "0.96",
-      ImageURL: "../assets/images/hammer.jpg"
+      imageURL: "../assets/images/hammer.jpg"
     }
     ,
     {
+      id: 4,
       productName: "Saw",
       code: "TBX-0022",
       available: "Mar 15, 2016",
       price: "11.55",
       starRating: "0.78",
-      ImageURL: "../assets/images/saw.jpg"
+      imageURL: "../assets/images/saw.jpg"
     },
     {
+      id: 5,
       productName: "Video Game Controller",
       code: "GMG-0042",
       available: "Oct 15, 2015",
       price: "35.95",
       starRating: "0.9",
-      ImageURL: "../assets/images/video-game-controller.jpg"
+      imageURL: "../assets/images/video-game-controller.jpg"
     }
 
   ]
