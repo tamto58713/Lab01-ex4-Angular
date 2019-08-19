112d79080bd467a1c6e5ba644b355e2d0c05b684
Tam To Tran <tamtotran5667@gmail.com>
2019-08-20 00:56:35 +0700

    edit path server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/src/app>'));
+app.use(express.static(__dirname + '/dist/lap01'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/app/app.component.html'));
+res.sendFile(path.join(__dirname+'/dist/lap01/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
